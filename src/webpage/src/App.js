56251983fb5076874c71697d6e17119b8efb5a6f
KiwiThePoodle <42877043+KiwiThePoodle@users.noreply.github.com>
2022-10-05 14:02:26 -0400

    combined searchbar to webpage

--- src/webpage/src/App.js
+++ src/webpage/src/App.js
@@ -1,20 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react"
-import { Route, Routes } from "react-router-dom";
-import {BrowserRouter as Router}  from "react-router-dom";
-import Create from './components/create';
+import "./index.css";
+import "./components/Searchbar";
+import SearchBar from "./components/Searchbar";
+
+import axios from "axios";
+
+import { message, Table } from "antd";
+import React, { useState } from "react";
+
 
 function App() {
+  const [datasource,setDatasource]=useState([])
+  const columns=[
+    {
+      title: 'School Name',
+      dataIndex: 'SchoolName',
+      key: 'SchoolName',
+    },
+    {
+      title: 'Offering Name',
+      dataIndex: 'OfferingName',
+      key: 'OfferingName',
+    },
+    {
+      title: 'Title',
+      dataIndex: 'Title',
+      key: 'Title',
+    },
+    {
+      title: 'Credits',
+      dataIndex: 'Credits',
+      key: 'Credits',
+    },
+    {
+      title: 'Instructors',
+      dataIndex: 'Instructors',
+      key: 'Instructors',
+    },
+  ]
+  function onSubmit(values){
+    const { CourseTitle, courseID } = values;
+    //
+    const opt = {
+      method: "GET",
+      params:{CourseTitle,courseID},
+      url: `/api/classes?key=9N7pYLkUVHSf8xzgFWMK5Cv7jnmQAzFo`,
+    };
+    axios(opt)
+      .then((res) => {
+        if (res.status === 200) {
+          setDatasource(res.data)
+          // message.success("Login succeed! ");
+        }
+      })
+      .catch((err) => {
+        console.log("failed: ", err.message);
+      });
+}
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Create />} />
-        </Routes>
-      </Router>
+    <div className="App">
+      <SearchBar  onFinish={onSubmit}/>
+      <Table dataSource={datasource} columns={columns}/>
     </div>
   );
 }
-
 export default App;
