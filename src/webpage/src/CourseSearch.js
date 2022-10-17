b8d090e2506ed93d78de40ffaa966273fe33851d
xiaoqin001 <xiaoqin6787@outlook.com>
2022-10-17 04:02:16 -0400

    add component CourseTable

--- src/webpage/src/CourseSearch.js
+++ src/webpage/src/CourseSearch.js
@@ -1,84 +1,18 @@
 import "./index.css";
 import "./components/Searchbar";
 import "./components/WriteReview";
+import "./components/CourseTable";
 import SearchBar from "./components/Searchbar";
-import WriteReview from "./components/WriteReview";
-import CourseSummary from "./components/CourseSummary";
-import ReadReview from "./components/ReadReviews";
+import CourseTable from "./components/CourseTable";
 import axios from "axios";
-import { message, Table, Card, Row, Col, Icon} from "antd";
+
 import React, { useState } from "react";
 
 function CourseSearch() {
 
-  const [datasource,setDatasource]=useState([])
-  const [keys,expandedKeys]=useState([])
-  const [activeTabKey, setActiveTabKey] = useState('ClassPrompt');
-  const [expandedRowKeys, setExpandedRowKeys] = useState([]);
-
-  const [recordValue, setRecord] = useState([]);
-
-  const [classInfo, setClassInfo]=useState([])
+  const [datasource, setDatasource]=useState([])
 
 
-  const columns=[
-    {
-      title: 'School Name',
-      dataIndex: 'SchoolName',
-      key: 'SchoolName',
-    },
-    {
-      title: 'Offering Name',
-      dataIndex: 'OfferingName',
-      key: 'OfferingName',
-    },
-    {
-      title: 'Title',
-      dataIndex: 'Title',
-      key: 'Title',
-    },
-    {
-      title: 'Credits',
-      dataIndex: 'Credits',
-      key: 'Credits',
-    },
-    {
-      title: 'Instructors',
-      dataIndex: 'Instructors',
-      key: 'Instructors',
-    }, {
-      title: 'ID',
-      dataIndex: 'SSS_SectionsID',
-      key: 'SSS_SectionsID',
-      hidden: true
-    }
-  ]
-
-  const tabListSkeleton = [
-    {
-      key: 'ClassPrompt',
-      tab: 'Class Summary',
-    },
-    {
-      key: 'AddAReview',
-      tab: 'Add A Review',
-    },
-    {
-      key: 'ViewAllReviews',
-      tab: 'View All Reviews',
-    },
-  ];
-
-  const contentList: Record<string, React.ReactNode> = {
-    ClassPrompt: <CourseSummary record = {classInfo}/>,
-    AddAReview: <WriteReview record = {recordValue}/>,
-    ViewAllReviews:<ReadReview record = {classInfo}/>
-  };
-
-  
-  const onTab2Change = (key) => {
-    setActiveTabKey(key);
-  };
 
   function onSubmit(values){
     const { CourseTitle, CourseNumber, Credits, Department } = values;
@@ -100,48 +34,12 @@ function CourseSearch() {
       });
 }
 
-const onTableRowExpand = (expanded, record) => {
-  const keys = [];
-  if(expanded){
-      keys.push(record.SSS_SectionsID); // I have set my record.id as row key. Check the documentation for more details.
-  }
 
 
-  setRecord(record.SSS_SectionsID);
-  setClassInfo(record);
-  setExpandedRowKeys(keys);
-  setActiveTabKey('ClassPrompt')
-}
-
-  
   return (
     <div className="App">
       <SearchBar  onFinish={onSubmit}/>
-      <Table
-      dataSource={datasource}
-      columns={columns.filter(col => col.title !== 'ID')}
-      rowKey = "SSS_SectionsID"
-      expandable={{
-        expandedRowRender: record => <Card
-        style={{ width: '100%' }}
-        tabList={tabListSkeleton}
-        activeTabKey={activeTabKey}
-
-        onTabChange={key => {
-          onTab2Change(key);
-          setRecord(record.SSS_SectionsID);
-          setClassInfo(record);
-          
-        }}
-      >
-        {contentList[activeTabKey]}
-      </Card>,
-        rowExpandable: record => record.OfferingName + record.SchoolName + record.Title + record.Instructors !== 'Not Expandable',
-      }}
-      expandedRowKeys={expandedRowKeys}
-      onExpand={onTableRowExpand}
-
-      />
+      <CourseTable data={datasource}/>
     </div>
   );
 }
---
+++ src/webpage/src/components/CourseTable.js
@@ -0,0 +1,117 @@
+import WriteReview from "./WriteReview";
+import CourseSummary from "./CourseSummary";
+import ReadReview from "./ReadReviews";
+import { useState } from "react";
+import { Table, Card } from "antd";
+import { OmitProps } from "antd/lib/transfer/ListBody";
+
+const CourseTable = (props) => {
+    const [keys,expandedKeys]=useState([]);
+    const [activeTabKey, setActiveTabKey] = useState('ClassPrompt');
+    const [expandedRowKeys, setExpandedRowKeys] = useState([]);
+    const [recordValue, setRecord] = useState([]);
+    const [classInfo, setClassInfo]=useState([]);
+
+
+    const columns=[
+        {
+          title: 'School Name',
+          dataIndex: 'SchoolName',
+          key: 'SchoolName',
+        },
+        {
+          title: 'Offering Name',
+          dataIndex: 'OfferingName',
+          key: 'OfferingName',
+        },
+        {
+          title: 'Title',
+          dataIndex: 'Title',
+          key: 'Title',
+        },
+        {
+          title: 'Credits',
+          dataIndex: 'Credits',
+          key: 'Credits',
+        },
+        {
+          title: 'Instructors',
+          dataIndex: 'Instructors',
+          key: 'Instructors',
+        }, {
+          title: 'ID',
+          dataIndex: 'SSS_SectionsID',
+          key: 'SSS_SectionsID',
+          hidden: true
+        }
+      ]
+
+      const tabListSkeleton = [
+        {
+          key: 'ClassPrompt',
+          tab: 'Class Summary',
+        },
+        {
+          key: 'AddAReview',
+          tab: 'Add A Review',
+        },
+        {
+          key: 'ViewAllReviews',
+          tab: 'View All Reviews',
+        },
+      ];
+
+    const contentList = {
+        ClassPrompt: <CourseSummary record = {classInfo}/>,
+        AddAReview: <WriteReview record = {recordValue}/>,
+        ViewAllReviews:<ReadReview record = {classInfo}/>
+    };
+
+
+    const onTab2Change = (key) => {
+        setActiveTabKey(key);
+    };
+
+    const onTableRowExpand = (expanded, record) => {
+        const keys = [];
+        if(expanded){
+            keys.push(record.SSS_SectionsID); // I have set my record.id as row key. Check the documentation for more details.
+        }
+
+
+        setRecord(record.SSS_SectionsID);
+        setClassInfo(record);
+        setExpandedRowKeys(keys);
+        setActiveTabKey('ClassPrompt')
+    }
+
+    return (
+        <Table
+            dataSource={props.data}
+            columns={columns.filter(col => col.title !== 'ID')}
+            rowKey = "SSS_SectionsID"
+            expandable={{
+            expandedRowRender: record => <Card
+            style={{ width: '100%' }}
+            tabList={tabListSkeleton}
+            activeTabKey={activeTabKey}
+
+            onTabChange={key => {
+                onTab2Change(key);
+                setRecord(record.SSS_SectionsID);
+                setClassInfo(record);
+            }}
+            >
+
+            {contentList[activeTabKey]}
+            </Card>,
+            rowExpandable: record => record.OfferingName + record.SchoolName + record.Title + record.Instructors !== 'Not Expandable',
+            }}
+            expandedRowKeys={expandedRowKeys}
+            onExpand={onTableRowExpand}
+        />
+    );
+
+}
+
+export default CourseTable;
