83821ce527d7d52ea524fc2a0f8ecd73c4656cca
xiaoqin001 <xiaoqin6787@outlook.com>
2022-10-28 17:20:30 -0400

    small changes on search.js

--- src/server/routes/search.js
+++ src/server/routes/search.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { cloneElement } = require("react");
 const dbo = require("../db/conn_search");
 const ObjectId = require("mongodb").ObjectId;
 
@@ -12,23 +13,28 @@ searchRoutes.route("/courselist").get(function (req, res) {
 
   let myquery = {};
 
+  if (!req.query.CourseTitle && !req.query.CourseNumber && !req.query.Credits && !req.query.Department){
+    Object.assign(myquery,{'Title': 'false'})
+  } else {
+    if (req.query.CourseTitle) {
+      Object.assign(myquery,{'Title':{$regex:req.query.CourseTitle,$options: 'i'}})
+    }
+    if (req.query.CourseNumber) {
+      Object.assign(myquery,{'OfferingName':{$regex:req.query.CourseNumber,$options: 'i'}})
+    }
+    if (req.query.Credits) {
+      Object.assign(myquery,{'Credits':{$regex:req.query.Credits,$options: 'i'}})
+    }
+    // if (req.query.InstructorsFullName) {
+    //   Object.assign(myquery,{'InstructorsFullName':{$regex:req.query.InstructorsFullName,$options: 'i'}})
+    // }
+    if (req.query.Department) {
+      Object.assign(myquery,{'Department':{$regex:req.query.Department,$options: 'i'}})
+      // Object.assign(myquery,{'Department':req.query.Department})
+    }
+  }
 
-  if (req.query.CourseTitle) {
-    Object.assign(myquery,{'Title':{$regex:req.query.CourseTitle,$options: 'i'}})
-  }
-  if (req.query.CourseNumber) {
-    Object.assign(myquery,{'OfferingName':{$regex:req.query.CourseNumber,$options: 'i'}})
-  }
-  if (req.query.Credits) {
-    Object.assign(myquery,{'Credits':{$regex:req.query.Credits,$options: 'i'}})
-  }
-  // if (req.query.InstructorsFullName) {
-  //   Object.assign(myquery,{'InstructorsFullName':{$regex:req.query.InstructorsFullName,$options: 'i'}})
-  // }
-  if (req.query.Department) {
-    Object.assign(myquery,{'Department':{$regex:req.query.Department,$options: 'i'}})
-    // Object.assign(myquery,{'Department':req.query.Department})
-  }
+
 
 
   db_connect.collection("classes").countDocuments(myquery).then((total) =>{
