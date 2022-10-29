fcae457ae5c6025e7f7eb7648b18ed14da547b2d
echua3 <epiphany.chua3@gmail.com>
2022-10-28 22:56:26 -0400

    title and welcome

--- src/webpage/src/index.js
+++ src/webpage/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import CourseSearch from "./CourseSearch";
+import Home from "./Home";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<CourseSearch />, rootElement);
+//ReactDOM.render(<CourseSearch />, rootElement);
+ReactDOM.render(<Home />, rootElement);
