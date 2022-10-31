5f7c7087a1137cb3abd7e5c02acbb995d00e930b
xiaoqin001 <xiaoqin6787@outlook.com>
2022-10-31 14:50:39 -0400

    simple homepage UI

--- src/webpage/src/Pages/HomePage.js
+++ src/webpage/src/Pages/HomePage.js
@@ -1,12 +1,24 @@
 import "../index.css";
-
 import React from "react";
+import HomepageCard from "../components/HomepageCard";
 
 function HomePage() {
 
   return (
     <div className="HomePage">
-      Welcome to HopCourses!
+
+      <h1 className='title'>Welcome to HopCourses</h1>
+      <div className="homepage">
+        <img
+          className="homepage_img2"
+          src="homepagePic3.jpg"
+          alt="HopCourses"
+        />
+      </div>
+      {/* <div>
+        <HomepageCard />
+      </div> */}
+
     </div>
   );
 }
