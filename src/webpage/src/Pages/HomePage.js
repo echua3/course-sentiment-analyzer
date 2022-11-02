44be8045481f13338a42785f3668ec524aee938c
echua3 <epiphany.chua3@gmail.com>
2022-11-02 08:34:42 -0400

    homepage

--- src/webpage/src/Pages/HomePage.js
+++ src/webpage/src/Pages/HomePage.js
@@ -1,19 +1,25 @@
 import "../index.css";
 import React from "react";
+import "./Style/Homepage.css";
 import HomepageCard from "../components/HomepageCard";
 
 function HomePage() {
+  const imageName = 'homepagePic3.jpg';
 
   return (
     <div className="HomePage">
 
       <h1 className='title'>Welcome to HopCourses</h1>
       <div className="homepage">
-        <img
+        <div style={{ backgroundImage: `url('/${imageName}'})` }}>
+        </div>
+        {/* <img src={`/${imageName}`} /> */}
+        
+        {/* <img
           className="homepage_img2"
           src="homepagePic3.jpg"
           alt="HopCourses"
-        />
+        /> */}
       </div>
       {/* <div>
         <HomepageCard />
