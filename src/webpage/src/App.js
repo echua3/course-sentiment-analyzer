387576d243632ae1faff32e5d896d3335de63ae0
kunalkotkar10 <kunal.kotkar2000@gmail.com>
2022-11-02 14:43:16 -0400

    changes to new branch for merge

--- src/webpage/src/App.js
+++ src/webpage/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Navigation/Navbar.js";
 import HomePage from "./Pages/HomePage.js";
 import CourseSearch from "./Pages/CourseSearch.js";
 import Recommendations from "./Pages/Recommendations.js";
+import UserProfile from "./Pages/UserProfile.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/Courses" element={<CourseSearch />} />
           <Route path="/Recommendations" element={<Recommendations />} />
+          <Route path="/Profile" element={<UserProfile />} />
         </Routes>
       </div>
     </Router>
--- src/webpage/src/Navigation/Navbar.js
+++ src/webpage/src/Navigation/Navbar.js
@@ -102,7 +102,7 @@ export default function Navbar() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="/Profile"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
