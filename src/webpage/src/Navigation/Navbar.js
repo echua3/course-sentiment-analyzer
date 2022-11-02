faa26bc414208090f9bf1c096988938749385297
KiwiThePoodle <42877043+KiwiThePoodle@users.noreply.github.com>
2022-11-02 04:40:47 -0400

    fixed navigation reload bug

--- src/webpage/src/Navigation/Navbar.js
+++ src/webpage/src/Navigation/Navbar.js
@@ -1,14 +1,11 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import "./output.css";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
-
-const navigation = [
-  { name: "Homepage", href: "/", current: false },
-  { name: "Courses", href: "/Courses", current: false },
-  { name: "Recommendations", href: "/Recommendations", current: false },
-];
+import Recommendations from "../Pages/Recommendations";
+import CourseSearch from "../Pages/CourseSearch";
+import HomePage from "../Pages/HomePage";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -50,25 +47,22 @@ export default function Navbar() {
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "px-3 py-2 rounded-md text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+                    <CustomLink key={HomePage} href={"/"} className={classNames(
+                        "text-gray-300 hover:bg-gray-700 hover:text-white",
+                        "px-3 py-2 rounded-md text-sm font-medium"
+                      )} to="/">HomePage</CustomLink>
+                    <CustomLink key={CourseSearch} href={"/Courses"} className={classNames(
+                        "text-gray-300 hover:bg-gray-700 hover:text-white",
+                        "px-3 py-2 rounded-md text-sm font-medium"
+                      )} to="/Courses">Courses</CustomLink>
+                    <CustomLink key={Recommendations} href={"/Recomendations"} className={classNames(
+                        "text-gray-300 hover:bg-gray-700 hover:text-white",
+                        "px-3 py-2 rounded-md text-sm font-medium"
+                      )} to="/Recommendations">Recommendations</CustomLink>
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 space-x-4">
                 <button type="button" className="bg-gray-800 p-1 rounded-full text-gray-300 hover:text-white">
                   Login
                 </button>
@@ -81,7 +75,7 @@ export default function Navbar() {
                 {/* Profile dropdown */}
                 <Menu as="div" className="ml-3 relative">
                   <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full">
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
@@ -135,22 +129,20 @@ export default function Navbar() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  <CustomLink to="/{item.name}">{item.name}</CustomLink>
-                </Disclosure.Button>
-              ))}
+              <div className="flex space-x-4">
+                <CustomLink key={HomePage} href={"/"} className={classNames(
+                    "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "px-3 py-2 rounded-md text-sm font-medium"
+                  )} to="/">HomePage</CustomLink>
+                <CustomLink key={CourseSearch} href={"/Courses"} className={classNames(
+                    "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "px-3 py-2 rounded-md text-sm font-medium"
+                  )} to="/Courses">Courses</CustomLink>
+                <CustomLink key={Recommendations} href={"/Recomendations"} className={classNames(
+                    "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "px-3 py-2 rounded-md text-sm font-medium"
+                  )} to="/Recommendations">Recommendations</CustomLink>
+              </div>
             </div>
           </Disclosure.Panel>
         </>
