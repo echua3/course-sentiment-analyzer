59c2c7ae53f73b26ed4826867e705a9b7a1c57fe
echua3 <epiphany.chua3@gmail.com>
2022-11-05 16:36:57 -0400

    deployment changes

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 require("dotenv").config({ path: "./config.env"})
 const port = process.env.PORT || 3000;
 
+const path = require("path");
+
 app.use(cors());
 app.use(express.json());
 addReviewRoute = require("./routes/addReview")
@@ -17,6 +19,9 @@ app.use('/api', searchCourseRoute)
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+// Pick up React index.html file
+app.use(express.static(path.join(__dirname, "../webpage/build")))
 
 const dbo_search = require("./db/conn_search")
 
