6ac3a4d0f5c2695fa9b5add77049c20372128c60
James Yu <42877043+KiwiThePoodle@users.noreply.github.com>
2022-11-10 13:07:56 -0500

    Navbar changes to fix deployed profile not working

--- src/webpage/src/Navigation/Navbar.js
+++ src/webpage/src/Navigation/Navbar.js
@@ -1,4 +1,3 @@
-import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import "./output.css";
@@ -6,7 +5,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import Recommendations from "../Pages/Recommendations";
 import CourseSearch from "../Pages/CourseSearch";
 import HomePage from "../Pages/HomePage";
-import App from "../App";
+import UserProfile from "../Pages/UserProfile"
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -64,60 +63,19 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 space-x-4">
-                <button type="button" className="bg-gray-800 p-1 rounded-full text-gray-300 hover:text-white">
-                  Login
-                </button>
-                {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full">
-                      <span className="sr-only">Open user menu</span>
-                      <img
+                <CustomLink key={UserProfile} href={"/Profile"} className={classNames(
+                        "bg-gray-800 flex text-sm rounded-full",
+                        "block px-4 py-2 text-sm text-gray-700"
+                )} to="/Profile">
+                  <img
                         className="h-8 w-8 rounded-full"
                         src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png"
                         alt=""
                       />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="/Profile"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                </CustomLink>
+                <button type="button" className="bg-gray-800 p-1 rounded-full text-gray-300 hover:text-white">
+                  Logout
+                </button>
               </div>
             </div>
           </div>
