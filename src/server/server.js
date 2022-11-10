bd67e99521ca4976c4c09f3cf7a1867b8fc4b9de
echua3 <epiphany.chua3@gmail.com>
2022-11-10 16:04:54 -0500

    fix directory path

--- src/server/server.js
+++ src/server/server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../webpage/build/index.html'));
 });
 
 const dbo_search = require("./db/conn_search")
