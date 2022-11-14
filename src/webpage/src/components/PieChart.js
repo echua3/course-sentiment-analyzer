5fb402748da901ee99dbeb82a4b841564cc756bb
kunalkotkar10 <kunal.kotkar2000@gmail.com>
2022-11-14 17:56:47 -0500

    added piechart js

---
+++ src/webpage/src/components/PieChart.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Pie} from 'react-chartjs-3/auto'
+
+function PieChart(){
+      
+    // const data = {
+    //     labels: [
+    //         'Red',
+    //         'Green',
+    //         'Yellow'
+    //     ],
+    //     datasets: [{
+    //         data: [300, 50, 100],
+    //         backgroundColor: [
+    //         '#FF6384',
+    //         '#36A2EB',
+    //         '#FFCE56'
+    //         ],
+    //         hoverBackgroundColor: [
+    //         '#FF6384',
+    //         '#36A2EB',
+    //         '#FFCE56'
+    //         ]
+    //     }]
+    // };
+
+    return (
+        <div>
+          <h2>Pie Example</h2>
+        </div>
+      );
+} 
+    
+export default PieChart;
