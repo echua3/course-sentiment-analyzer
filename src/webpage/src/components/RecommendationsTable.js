540cdb86b505df1e68c38bdeb889c411fa1d3a70
KiwiThePoodle <42877043+KiwiThePoodle@users.noreply.github.com>
2022-11-14 22:34:16 -0500

    recommendations functional

--- src/webpage/src/components/RecommendationsTable.js
+++ src/webpage/src/components/RecommendationsTable.js
@@ -9,12 +9,15 @@ function RecommendationsTable(props) {
 
   const [datasource, setDatasource] = useState([]);
   const [user, setUser] = useState("");
+  const [loadings, setLoadings] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
-
   useEffect( () => {
     async function getRecords() {
-        await fetch(API_ENDPOINT + "/user/" + "jy6").then(async(response) => {
+        startLoading(0);
+        setLoading(true);
+        await fetch(API_ENDPOINT + "/user/" + "af3").then(async(response) => {
         if (!response.ok) {
             const message = "An error occured"
             console.log("Error:" + response.statusText);
@@ -22,31 +25,72 @@ function RecommendationsTable(props) {
           }
           const records = await response.json();
           setUser(records.data[0]);
-          
-          const interests = await fetch(API_ENDPOINT + "/recs/" + "?firstInterest=" + records.data[0].firstInterest + "&secondInterest=" + records.data[0].secondInterest + "&thirdInterest=" + records.data[0].thirdInterest + "&department=" + records.data[0].department);
+
+          let first = "*";
+          let second = "*";
+          let third = "*";
+
+          if (records.data[0].firstInterest != "") {
+            first = records.data[0].firstInterest;
+          }
+          if (records.data[0].secondInterest != "") {
+            second = records.data[0].secondInterest;
+          }
+          if (records.data[0].thirdInterest != "") {
+            third = records.data[0].thirdInterest;
+          }
+
+          const interests = await fetch(API_ENDPOINT + "/recs/" + "?firstInterest=" + first + "&secondInterest=" + second + "&thirdInterest=" + third + "&department=" + records.data[0].department);
           if(!interests.ok) {
               const message = "An error occured"
               console.log("Error:" + interests.statusText);
               return;
           }
-          //setDatasource(interests);
           const recommendations = await interests.json();
-          console.log(recommendations);
-          //setDatasource(recommendations);
+          console.log(recommendations.data);
+          setDatasource(recommendations.data);
         })
-       
+        endLoading(0);
+        setLoading(false);
     }
     getRecords();
     
     return;
   }, []);
 
-  return (
+  const startLoading = (index) => {
+    setLoadings((prevLoadings) => {
+      const newLoadings = [...prevLoadings];
+      newLoadings[index] = true;
+      return newLoadings;
+    });
+  }
+
+  const endLoading = (index) => {
+    setLoadings((prevLoadings) => {
+      const newLoadings = [...prevLoadings];
+      newLoadings[index] = false;
+      return newLoadings;
+    });
+  }
+
+  if(isLoading) {
+    return (
     <div class='recommendationsPage'>
-        Your course recommendations are listed below! Please make sure to keep your interests up to date in your profile for accurate recommendations!
-      <CourseTable data={datasource}/>
+        <Button class='writereview-form-btn' type="primary" htmlType="submit" loading={loadings[0]}></Button>
     </div>
   );
+  } else {
+    return (
+        <div class='recommendationsPage'>
+            <text>
+                Your course recommendations are listed below! Please make sure to keep your interests up to date in your profile for accurate recommendations!
+            </text>
+            <CourseTable data={datasource}/>  
+        </div>
+      );
+  }
+  
 }
 
 
