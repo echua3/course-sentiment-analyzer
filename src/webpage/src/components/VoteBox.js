96348d1bef7265072d5412dc30d8fc814aeb3534
echua3 <epiphany.chua3@gmail.com>
2022-11-14 23:04:42 -0500

    types of voting handling

--- src/webpage/src/components/VoteBox.js
+++ src/webpage/src/components/VoteBox.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import { makeStyles } from '@material-ui/core/styles'
 import ArrowDropDown from '@material-ui/icons/ArrowDropDown'
 import ArrowDropUp from '@material-ui/icons/ArrowDropUp'
+import Upvote from "./Upvote";
  
  
 const useStyles = makeStyles((theme) => ({
@@ -27,9 +28,56 @@ const useStyles = makeStyles((theme) => ({
    },
  },
 }))
+
  
-const VoteBox = ({ votes, handleUpvote, handleDownvote }) => {
- const classes = useStyles()
+const VoteBox = ({ review, votes, handleUpvote, handleDownvote }) => {
+    const classes = useStyles()
+
+    const [clickedUp, setClickedUp] = useState();
+    const [clickedDown, setClickedDown] = useState();
+    let startingVotes = review.helpfulness;
+    const [currentVotes, setVotes] = useState(review.helpfulness);
+
+    if (!handleUpvote) {
+        handleUpvote = () => { 
+            if (clickedUp){
+                // already upvoted, undo upvote
+                setVotes(currentVotes - 1); 
+                setClickedUp(false);
+            } else if (clickedDown) {
+                // already downvoted, undo downvote
+                setVotes(currentVotes + 1); 
+                setClickedDown(false);
+            }
+            else{
+                // upvote
+                setVotes(startingVotes + 1);
+                setClickedUp(true);
+            }
+        };
+    }
+    console.log("votes: ", votes);
+    console.log("clickedUp: ", clickedUp);
+
+    if (!handleDownvote) {
+        handleDownvote = () => { 
+            if (clickedDown){
+                // already downvoted, undo downvote
+                setVotes(currentVotes + 1); 
+                setClickedDown(false);
+            } else if (clickedUp) {
+                // already upvoted, undo upvote
+                setVotes(currentVotes - 1);
+                setClickedUp(false);
+            }
+            else{
+                // downvote
+                setVotes(startingVotes - 1);
+                setClickedDown(true);
+            }
+        };
+    }
+
  return (
    <Grid
      container
@@ -40,11 +88,13 @@ const VoteBox = ({ votes, handleUpvote, handleDownvote }) => {
      data-testid="voteBox-container"
    >
      <IconButton onClick={handleUpvote} size="small" data-testid="upvote-button">
-       <ArrowDropUp className={classes.voteBox} />
+        {clickedUp ? <ArrowDropUp className={classes.voteBox} style={{fill: "blue"}}/> : <ArrowDropUp className={classes.voteBox}/>}
      </IconButton>
-     <Typography className={classes.votes} data-testid="voteBox-votes">{votes}</Typography>
+     <Typography className={classes.votes} data-testid="voteBox-votes">
+        {clickedUp || clickedDown ? currentVotes : review.helpfulness}
+    </Typography>
      <IconButton onClick={handleDownvote} size="small" data-testid="downvote-button">
-       <ArrowDropDown className={classes.voteBox} />
+     {clickedDown ? <ArrowDropDown className={classes.voteBox} style={{fill: "red"}}/> : <ArrowDropDown className={classes.voteBox}/>}
      </IconButton>
    </Grid>
  )
