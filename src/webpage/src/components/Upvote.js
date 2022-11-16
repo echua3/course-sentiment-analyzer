43ef930c58f22f6efdcf2513ce5e1c78ae3375cf
echua3 <epiphany.chua3@gmail.com>
2022-11-15 21:02:27 -0500

    clean comments

--- src/webpage/src/components/Upvote.js
+++ src/webpage/src/components/Upvote.js
@@ -16,37 +16,12 @@ function Upvote({reviewID}) {
         });
 
         if(!response.ok) {
-            // const records = await response.json();
-            // console.log(records);
-            // const basicMessage = records.error.message.split(/:(.*)/s)
-            // const allErrors = basicMessage[1].split(",")
-            // console.log(allErrors)
-            // allErrors.forEach(runErrorMessaging);
             console.log('!response.ok')
         } else {
             console.log(response)
         }
     }
     requestData();
-
-
-    //     useEffect( () => {
-    //     async function upvote() {
-    //         const response = await fetch(process.env.REACT_APP_API_ENDPOINT + "/review/upvote/" + {record}._id)
-    //         if(!response.ok) {
-    //           const message = "An error occurred"
-    //           console.log("Error:" + response.statusText);
-    //           return;
-    //         }
-    //          console.log(response);
-    //         const records = await response.json();
-    //         console.log(records.data);
-            
-    //     }   
-    //     upvote();
-
-    //     return;
-    // }, [recordValues.length]);
 
 }
 
