de50d0690dd6d75a4ceb1776b44ed353e6e3ae21
echua3 <epiphany.chua3@gmail.com>
2022-11-16 02:10:06 -0500

    fix average sentiment and difficulty display

--- src/webpage/src/components/CourseSummary.js
+++ src/webpage/src/components/CourseSummary.js
@@ -84,10 +84,10 @@ function CourseSummary({record}) {
          {Title}
     </span>
     <p>
-      Average Sentiment: {average_sentiment_score != null ? average_sentiment_score : 'N/A'}
+      Average Sentiment: {!isNaN(average_sentiment_score) ? average_sentiment_score : 'N/A'}
     </p>
     <p> 
-      Average Difficulty: {average_difficulty != null ? average_difficulty.toLocaleString(undefined, {maximumFractionDigits:2}) : 'N/A'}
+      Average Difficulty: {!isNaN(average_difficulty) ? average_difficulty.toLocaleString(undefined, {maximumFractionDigits:2}) : 'N/A'}
     </p>
       
     </Col>
