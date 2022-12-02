8e8278c98f7f320b833d7d3ba4592d5536310692
echua3 <epiphany.chua3@gmail.com>
2022-12-02 15:14:53 -0500

    reformat downvote route to try-catch

--- src/server/routes/downvoteReview.js
+++ src/server/routes/downvoteReview.js
@@ -15,81 +15,45 @@ const { param, validationResult } = require('express-validator');
 
 downvoteReviewRoute.route("/review/downvote/:reviewID/:userID").post(param('reviewID').trim().not().isEmpty(),
 param('userID').trim().not().isEmpty(),
-    function (req, res) {
+    async function (req, res) {
 
     let db_connect = dbo.getDb();
 
-    async.parallel([
+    var downvoteResult;
+    var userDownvoteResult;
 
-        function(callback) {
-            reviewModel.findByIdAndUpdate(req.params.reviewID, 
-                {$inc: {'helpfulness': -1}}
-            , function(err, results) {
-                if(err) return callback(err);
-                callback(null, results);
-            })
-        },
-        function(callback) {
-            userModel.updateOne({'userID': req.params.userID}, {
-                $addToSet: {
-                    ['reviewDownvoteIDs']: req.params.reviewID
-                }
-            }, function(err, results) {
-                if(err) return callback(err);
-                callback(null, results);
-            })
-        }
+    // edit helpfulness in review model
+    try {
+        downvoteResult = await reviewModel.findByIdAndUpdate(req.params.reviewID,
+            {$inc: {'helpfulness': -1}})
+        // console.log("downvoteResult: ", downvoteResult);
+    } catch (err) {
+        return res.status(500).json({
+            message: "Error while downvoting.",
+            error: err,
+        });
+    }
 
-    ], function(err, results) {
-        console.log("ERR: ", err);
-        if(err) {
-            res.status(500).json({
-                error: err
-            });
-        } else {
-            res.status(200).json({
-                message: "Review and User reviewDownvoteIDs updated!",
-                results: results,
-            });            
-        }
-        console.log("RESULTS:", results);
+    // edit reviewDownvoteIDs in user model
+    try {
+        userDownvoteResult = await userModel.updateOne({'userID': req.params.userID}, {
+            $addToSet: {
+                ['reviewDownvoteIDs']: req.params.reviewID
+            }
+        }) 
+        // console.log("userDownvoteResult: ", userDownvoteResult);
+    } catch (err) {
+        return res.status(500).json({
+            message: "Error while downvoting and editing user downvoteIDs.",
+            error: err,
+        });
+    }
+
+    res.status(200).json({
+        message: "Review and User reviewDownvoteIDs updated!",
+        results: [downvoteResult, userDownvoteResult],
     });
-
-    // // decrement the helpfulness count in reviews
-    // reviewModel.findByIdAndUpdate(req.params.reviewID, 
-    //     {$inc: {'helpfulness': -1}}
-    // ).then(result => {
-    //     res.status(200).json({
-    //     message: "Review updated!",
-    //     results: result,
-    //     });
-    // })
-    // .catch(err => {
-    //     // console.log(err);
-    //     res.status(500).json({
-    //         error: err
-    //     });
-    //     return;
-    // });
-
-    // // add review id to user's reviewDownvoteIDs
-    // userModel.updateOne({'userID': req.params.userID}, {
-    //         $addToSet: {
-    //             ['reviewDownvoteIDs']: req.params.reviewID
-    //         }
-    //     }
-    // ).then(result => {
-    //     res.status(200).json({
-    //     message: "User reviewDownvoteIDs updated!",
-    //     results: result,
-    //     });
-    // })
-    // .catch(err => {
-    //     res.status(500).json({
-    //         error: err
-    //     });
-    //     return;
-    // });
+    res.end();
     
 });
 
