0981b221cf5bdc2207bb77697d971af2b007a5a2
Epiphany Chua <65085254+echua3@users.noreply.github.com>
2022-12-08 14:39:44 -0800

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -107,21 +107,10 @@
 // }
 const { ObjectId } = require("mongodb")
 const express = require("express");
-const passport = require("passport");
-const saml = require("passport-saml");
-const session = require("express-session");
-const bodyParser = require("body-parser");
-const fs = require("fs");
-
-// MongoDB
-const { ObjectId } = require("mongodb")
+const app = express();
 const proxy = require('express-http-proxy');
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env"})
-const path = require("path");
-var MongoDBStore = require('connect-mongodb-session')(session);
-const cookieParser = require('cookie-parser')
-
 const port = process.env.PORT || 3000;
 const path = require("path");
 const cookieParser = require('cookie-parser')
@@ -148,40 +137,16 @@ login = require("./routes/login")
 logout = require("./routes/logout")
 getRecsRoute = require("./routes/getRecs")
 
-// routes
-addReviewRoute = require("./routes/addReview")
-searchReviewRoutes = require("./routes/searchReview")
-searchAllReviewsRoutes = require("./routes/searchAllReviews")
-searchCourseRoute = require("./routes/searchCourse")
-addUserRoute = require("./routes/addUser")
-retrieveUserRoute = require("./routes/retrieveUser")
-editUserRoute = require("./routes/editUser")
-userReviewRoutes = require("./routes/userReview")
-deleteReviewRoutes = require("./routes/deleteReviewProfile")
-showProfileRoutes = require("./routes/showProfile")
-editreviewRoutes = require("./routes/editReview")
-userInfoRoutes = require("./routes/userInfo")
-currentUserRoute = require("./routes/getCurrentUser")
-login = require("./routes/login")
-logout = require("./routes/logout")
-getRecsRoute = require("./routes/getRecs")
-
-
-// Setup SAML strategy
-const samlStrategy = new saml.Strategy(
-  {
-    // config options here
-    entryPoint: JHU_SSO_URL,
-    issuer: SP_NAME,
-    callbackUrl: `${BASE_URL}/jhu/login/callback`,
-    decryptionPvk: PvK,
-    privateCert: PvK,
-    cert: PbK,  //needed to prevent "cert is required" error
-  },
-  (profile, done) => {
-    return done(null, profile);
-  }
-);
+// allow cross-origin interaction:
+// app.use(cors({
+//     credentials: true,
+//     origin: [
+//         process.env.REACT_APP_API_ENDPOINT,
+//         'http://localhost:5000',
+//         'http://jhu-courses.herokuapp.com',
+//         'https://jhu-courses.herokuapp.com'
+//     ],
+//   }));
 
 app.use(cors({
     origin: "http://localhost:3000",
@@ -191,10 +156,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser());
 
-// Serialize and deserialize user for passport
-passport.serializeUser(function (user, done) {
-    done(null, user);
-});
 
 app.use(addReviewRoute)
 app.use(addUserRoute)
@@ -221,58 +182,8 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../webpage/build")))
 }
 
-// for MongoDB
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}))
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser());
-
-app.use(addReviewRoute)
-app.use(addUserRoute)
-app.use(searchReviewRoutes)
-app.use(userReviewRoutes)
-app.use(userInfoRoutes)
-app.use(retrieveUserRoute)
-app.use(editUserRoute)
-app.use(deleteReviewRoutes)
-app.use(showProfileRoutes)
-app.use(searchAllReviewsRoutes)
-app.use('/api', searchCourseRoute)
-app.use(login)
-app.use(logout)
-app.use(currentUserRoute)
-app.use(getRecsRoute)
-app.use(editreviewRoutes)
-
-// Set up homepage route
-app.get("/", (req, res) => {
-    res.send("Test Home Page!");
-});
-
-const dbo_search = require("./db/conn_search")
-
-const sanitizeHTML = require('sanitize-html');
-const addUserRoutes = require("./routes/addUser");
-const upvoteReviewRoute = require("./routes/upvoteReview");
-const downvoteReviewRoute = require("./routes/downvoteReview");
-const undoUpvoteReviewRoute = require("./routes/undoUpvoteReview");
-const undoDownvoteReviewRoute = require("./routes/undoDownvoteReview");
-
-app.use(upvoteReviewRoute)
-app.use(downvoteReviewRoute)
-app.use(undoUpvoteReviewRoute)
-app.use(undoDownvoteReviewRoute)
-
-// Pick up React index.html file
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../webpage/build")))
-}
-
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../webpage/build/index.html'));
+    res.sendFile(path.join(__dirname, '../webpage/build/index.html'));
 });
 
 const dbo_search = require("./db/conn_search")
@@ -289,16 +200,14 @@ app.use(downvoteReviewRoute)
 app.use(undoUpvoteReviewRoute)
 app.use(undoDownvoteReviewRoute)
 
-// Start the server.
 app.listen(port, () => {
-  dbo_search.connectToServer(function (err) {
-      if (err) console.error(err);
-  })
 
-  console.log(`Server is running on port: ${port}`);
-    // console.log(`Server is running on port: ${port}`);
+    dbo_search.connectToServer(function (err) {
+        if (err) console.error(err);
+    })
+
+    console.log(`Server is running on port: ${port}`);
 })
-
 
 function cleanup(req, res, next) {
     if (typeof req.body.initialReview != "string") req.body.initialReview = ""
