9f5a9f23fa00733d1bdd323f6dfe62c32ac4da5b
xiaoqin001 <xiaoqin6787@outlook.com>
2022-12-14 18:16:23 -0500

    courseID deleted from user collection

--- src/server/routes/deleteReviewProfile.js
+++ src/server/routes/deleteReviewProfile.js
@@ -23,12 +23,23 @@ deleteReviewRoutes.route("/user/review/delete/:userID/:reviewID").delete(async f
   let finduser = await userModel.find({userID: req.params.userID}).exec();
                     // const onereview = await reviewModel.find({classID:729491}).exec();
 
+
+  let onereview = await reviewModel.find({_id: ObjectId(req.params.reviewID)}).exec();
+  let classid = onereview[0]['classID'];
+
   const reviewUpdate = finduser[0]['reviewIDs'].remove(req.params.reviewID);
+  console.log(finduser[0]['courseIDs'])
+  const classidUpdate = finduser[0]['courseIDs'].remove(classid);
+  console.log(classidUpdate)
+
 
   await reviewModel.findOneAndDelete({_id: ObjectId(req.params.reviewID)})
+  await userModel.findOneAndDelete()
   userModel.findOneAndUpdate(
     {userID: req.params.userID },
-    {reviewIDs: reviewUpdate}
+    {reviewIDs: reviewUpdate,
+     courseIDs: classidUpdate
+    }
     ).then(result => {
     res.status(200).json({
     message: "Profile updated!",
