604c8cb3e77974dced2e0e51e2c582e74e59a754
echua3 <epiphany.chua3@gmail.com>
2022-12-14 20:55:01 -0500

    update navbar for small screens

--- src/webpage/src/Navigation/Navbar.js
+++ src/webpage/src/Navigation/Navbar.js
@@ -110,15 +110,15 @@ export default function Navbar() {
               <div className="flex space-x-4">
                 <CustomLink key={HomePage} href={"/"} className={classNames(
                     "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "px-3 py-2 rounded-md text-sm font-medium"
+                    "px-2 py-2 rounded-md text-sm font-medium"
                   )} to="/">HomePage</CustomLink>
                 <CustomLink key={CourseSearch} href={"/Courses"} className={classNames(
                     "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "px-3 py-2 rounded-md text-sm font-medium"
+                    "px-2 py-2 rounded-md text-sm font-medium"
                   )} to="/Courses">Courses</CustomLink>
                 <CustomLink key={Recommendations} href={"/Recommendations"} className={classNames(
                     "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "px-3 py-2 rounded-md text-sm font-medium"
+                    "px-2 py-2 rounded-md text-sm font-medium"
                   )} to="/Recommendations">Recommendations</CustomLink>
               </div>
             </div>
